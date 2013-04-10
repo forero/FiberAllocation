5164c9d71f4e039778906367ed7ac224637efbd1
Jaime Forero <j.e.forero.romero@gmail.com>
2013-04-10 11:05:58 -0500

    latest version from SPOKES pipeline

--- source/AllocationModule.py
+++ source/AllocationModule.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-
+# reference: https://code.google.com/p/esutil/source/browse/trunk/esutil/htm/__init__.py
+#import esutil
 import numpy as np
 import sys
+import math
 
 class FiberSet(object):
     """
@@ -9,7 +11,7 @@ class FiberSet(object):
     Flat sky and cartesian positions are assumed
 
     Inputs:
-    Ndiameter: number of fiabers along the tile's diameter. 
+    Ndiameter: number of fiabers along the tile's diameter.
     Fiber_pitch: fiber_pitch in degrees
     Fiber_size: fiber_size in degrees
     center_x: fiber center in degrees
@@ -38,11 +40,11 @@ class FiberSet(object):
         self.fiber_pitch = fiber_pitch # This quantity has to be modif
         self.Ndiameter = Ndiameter
         self.make_hexagon_tile()
-            
+
     def make_hexagon_tile(self):
         self.Ndiameter = self.Ndiameter - (self.Ndiameter%2 -1) #makes the number of spines in the main axis an odd number
         self.radius = (self.Ndiameter - 1.0) * self.fiber_pitch / 2.0
-                
+
         x_baseline = np.arange(self.Ndiameter) * self.fiber_pitch
         x_baseline = x_baseline - np.median(x_baseline)
         y_baseline = np.zeros(self.Ndiameter, dtype=float)
@@ -55,7 +57,7 @@ class FiberSet(object):
 
         # first pass (dec positive direction)
         n_vertical =  ((self.Ndiameter-1) - ((self.Ndiameter-1)/2))
-        
+
         for i in range(n_vertical):
             new_n_points = self.Ndiameter - i -1
             new_x_line = np.zeros((new_n_points), dtype=float)
@@ -74,18 +76,27 @@ class FiberSet(object):
             new_y_line = y_baseline[0:self.Ndiameter-i-1] - (i+1) * (np.sqrt(3.0)/2.0) * self.fiber_pitch
             self.x = np.append(self.x, new_x_line)
             self.y = np.append(self.y, new_y_line)
+
+
+        #makes a rotation of 30 degrees to match the hexagon shape in surfey strategy
+        tmp_x = self.x.copy()
+        tmp_y = self.y.copy()
+        tmp_x = self.x * np.cos(30.0 * math.pi/180.0) - self.y * np.sin(30.0 * math.pi/180.0) 
+        tmp_y = self.x * np.sin(30.0 * math.pi/180.0) + self.y * np.cos(30.0 * math.pi/180.0) 
+
         
-        self.x[:] = self.x[:] + self.center_x
-        self.y[:] = self.y[:] + self.center_y
+
+        self.x = tmp_x + self.center_x
+        self.y = tmp_y + self.center_y
 
         self.original_x = self.x.copy()
         self.original_y = self.y.copy()
-        
-        
-        self.Nfibers = self.x.size    
+
+
+        self.Nfibers = self.x.size
         self.ID = np.arange(self.Nfibers)
         self.allocated_galaxy_ID = np.zeros(self.Nfibers)
-        self.allocated_galaxy_ID[:] = -1 
+        self.allocated_galaxy_ID[:] = -1
 
 # <codecell>
 
@@ -96,17 +107,17 @@ class MockGalaxyCatalog(object):
     def __init__(self,
                  radius_fov = 1.0,
                  number_density = 1000,
-                 galaxy_type_abundance = np.array([1.0]), 
+                 galaxy_type_abundance = np.array([1.0]),
                  center_x = 0.0,
-                 center_y = 0.0, 
+                 center_y = 0.0,
                  filename = "data.dat",
                  x_in = np.array([0.0]),
                  y_in = np.array([0.0]),
                  id_in = np.array([0]),
                  random = False
-                ):        
+                ):
         #if there is a filename to load do it
-        
+
         if(filename!="data.dat"):
             self.filename = filename
             self.load_galaxy_catalog()
@@ -114,10 +125,10 @@ class MockGalaxyCatalog(object):
             #basic intrisic properties of the galaxy distribution
             self.radius_fov = radius_fov
             self.Ngalaxies = np.int_(3.14159 * (self.radius_fov**2) * number_density)
-        
+
             self.galaxy_type_abundance = galaxy_type_abundance.copy()
             self.n_types = np.size(self.galaxy_type_abundance)
-            
+
             self.center_x = center_x
             self.center_y = center_y
             self.make_random_mock_catalog()
@@ -130,39 +141,43 @@ class MockGalaxyCatalog(object):
             self.center_y = np.mean(y_in)
 
         #properties related the fiber allocation
-        self.tile_visit_ID = np.zeros(self.Ngalaxies)
-        self.tile_visit_ID[:] = -1
+        self.tile_ID = np.zeros(self.Ngalaxies)
+        self.tile_ID[:] = -2
+        self.fiber_ID = np.zeros(self.Ngalaxies)
+        self.fiber_ID[:] = -2
+        self.visit_ID = np.zeros(self.Ngalaxies)
+        self.visit_ID[:] = -2
         self.fibers_per_galaxy = np.zeros(self.Ngalaxies)
         self.galaxies_around_fiber_size = np.zeros(self.Ngalaxies)
         self.galaxies_around_fiber_pitch = np.zeros(self.Ngalaxies)
-            
-        
+
+
     def load_galaxy_catalog(self):
         print "Loading", self.filename
         raw_data = np.loadtxt(self.filename)
         selected = raw_data[:,7]
 
-        
+
         self.x = raw_data[:,8]
         self.y = raw_data[:,9]
         self.ID = raw_data[:,1]
         self.Ngalaxies = np.size(self.ID)
-        
+
         self.gal_type = raw_data[:,0]
-        
+
         self.center_x = np.average(self.x)
         self.center_y = np.average(self.y)
         self.radius_fov = -1.0
         print np.shape(self.x)
-        
+
     def make_random_mock_catalog(self):
         radius_fov_rand = self.radius_fov * np.sqrt(np.random.rand(self.Ngalaxies)) # Radius in degrees
         theta_fov_rand = np.random.rand(self.Ngalaxies) * 2.0 * np. pi
-         
+
         self.ID = np.arange(self.Ngalaxies)
         self.x = radius_fov_rand * np.cos(theta_fov_rand)
         self.y = radius_fov_rand * np.sin(theta_fov_rand)
-        
+
         self.x[:] = self.x[:] + self.center_x
         self.y[:] = self.y[:] + self.center_y
 
@@ -201,24 +216,24 @@ def close_match_xy(tx, ty, fx, fy, t_id, f_id, f_per_target, t_per_target, t_clo
     id_match_t = np.arange(n_f)
     index_match_t = np.arange(n_f)
     distance_t_to_f = np.zeros(n_f)
-    
+
     #find the numer of fibers per target
     f_per_target = count_fibers_per_galaxy(tx, ty, fx, fy, epsilon, f_per_target)
     t_per_target = count_galaxies_per_galaxy(tx, ty, exclusion_epsilon, epsilon, t_per_target)
     t_close = count_galaxies_per_galaxy(tx, ty, 0.0, epsilon * 0.1, t_close)
-    
+
     #loop over the number of fibers to find the number of targets within radius epsilon
     for i in range(n_f):
         f_x = fx[i]
         f_y = fy[i]
         n_target = tx.size
         initial_id = np.copy(t_id)
-        
-        
+
+
         #find the index of the targets around epsilon of the fiber
         delta_radius = np.sqrt((tx - f_x)**2 + (ty - f_y)**2)
         index = np.where(delta_radius<epsilon)
-        
+
         #cut the targets to those close to the fibers
         id_close = initial_id[index]
         close_tx = tx[index]
@@ -228,7 +243,7 @@ def close_match_xy(tx, ty, fx, fy, t_id, f_id, f_per_target, t_per_target, t_clo
         galaxy_density = t_per_target[index]
         galaxy_in_fiber = t_close[index]
         index_close = index[0]
-    
+
         # sort the IDs by some criterion
         if(rank_criterion=="distance"):
             sorted_delta_radius = np.argsort(close_delta_radius)
@@ -236,14 +251,14 @@ def close_match_xy(tx, ty, fx, fy, t_id, f_id, f_per_target, t_per_target, t_clo
             sorted_delta_radius = np.argsort(fiber_per_target)
         elif(rank_criterion=="galaxy_density"):
             # hight prob indicates a high number density of targets, meaning that it is hard to get it with out collisions.
-            prob = np.maximum(galaxy_density/7.0, galaxy_in_fiber) 
-            priority = np.exp(-prob) 
+            prob = np.maximum(galaxy_density/7.0, galaxy_in_fiber)
+            priority = np.exp(-prob)
             sorted_delta_radius = np.argsort(priority)
         else:
             print "The priority criterion does not exist:", rank_criterion
             sys.exit()
-            
-            
+
+
         sorted_id = id_close[sorted_delta_radius]
         sorted_index = index_close[sorted_delta_radius]
         sorted_tx = close_tx[sorted_delta_radius]
@@ -255,7 +270,7 @@ def close_match_xy(tx, ty, fx, fy, t_id, f_id, f_per_target, t_per_target, t_clo
             distance_t_to_f[i] = close_delta_radius[sorted_delta_radius][0]
             fx[i] = sorted_tx[0]
             fy[i] = sorted_ty[0]
-#            print close_delta_radius[sorted_delta_radius][0]
+#            print "sorted", close_delta_radius[sorted_delta_radius][0]
         else:
             id_match_f[i] = f_id[i]
             id_match_t[i] = -1
@@ -271,7 +286,7 @@ def reset_fiber_collisions(id_t, f_x, f_y, f_x0, f_y0, epsilon=0.0):
     Fiber collisions are found if the distance between two fibers is less than epsilon
     f_x0, f_y0 are the unperturbed fiber positions.
     """
-    
+
     #find the cases of colliding fibers
     u = np.empty((0))
     test = np.copy(id_t)
@@ -292,16 +307,16 @@ def reset_fiber_collisions(id_t, f_x, f_y, f_x0, f_y0, epsilon=0.0):
             n_reset = n_reset+1
     print n_reset, 'fibers were reset'
     return n_reset
-            
+
 
 # <codecell>
 
-def make_fiber_allocation(fibers, gals, tile_visit_ID=1, patrol_radius=0.0, rank_criterion="distance", exclusion_radius=0.0):
+def make_fiber_allocation(fibers, gals, tile_ID=1, visit_ID=1, patrol_radius=0.0, rank_criterion="distance", exclusion_radius=0.0):
     max_n_iterations = 10
 
     #check that there are fibers to be allocated
     n_to_allocate = np.size(np.where(fibers.allocated_galaxy_ID==-1))
-    n_to_find = np.size(np.where(gals.tile_visit_ID==-1))
+    n_to_find = np.size(np.where(gals.tile_ID==-1))
     iteration = 0
     gal_assigned = 0
     old_n_to_allocate = 0
@@ -309,8 +324,8 @@ def make_fiber_allocation(fibers, gals, tile_visit_ID=1, patrol_radius=0.0, rank
         print 'All the fibers must be free before starting its allocation'
         sys.exit()
     while (n_to_allocate > 0 and n_to_find > 0 and iteration < max_n_iterations and old_n_to_allocate!=n_to_allocate):
-       # print 'There are ', n_to_allocate, 'fibers to allocate'
-       # print 'There are ', n_to_find, 'galaxies to find'
+#        print 'There are ', n_to_allocate, 'fibers to allocate'
+#        print 'There are ', n_to_find, 'galaxies to find'
         old_n_to_allocate = n_to_allocate
 
         # Initialize the arrays with the fibers that have to be allocated
@@ -321,9 +336,9 @@ def make_fiber_allocation(fibers, gals, tile_visit_ID=1, patrol_radius=0.0, rank
         fibers_x0 = fibers.original_x[to_allocate]
         fibers_y0 = fibers.original_y[to_allocate]
         fibers_ID = fibers.ID[to_allocate]
-        
+
         #initialize the arrays with galaxies that have not been observed
-        free_gals = np.where(gals.tile_visit_ID==-1)
+        free_gals = np.where(gals.tile_ID==-1)
         free_gals = free_gals[0]
         gals_x = gals.x[free_gals]
         gals_y = gals.y[free_gals]
@@ -331,14 +346,14 @@ def make_fiber_allocation(fibers, gals, tile_visit_ID=1, patrol_radius=0.0, rank
         fibers_per_galaxy = gals.fibers_per_galaxy[free_gals]
         galaxies_per_galaxy = gals.galaxies_around_fiber_pitch[free_gals]
         galaxies_per_fiber_size = gals.galaxies_around_fiber_size[free_gals]
-        
-        
-        # Make a pass of allocation 
+
+
+        # Make a pass of allocation
         alloc_id_fibers, id_matching_gals, index_matching_gals, distance_fiber_to_gal = \
         close_match_xy(gals_x, gals_y, fibers_x, fibers_y, gals_ID, fibers_ID, \
         fibers_per_galaxy, galaxies_per_galaxy, galaxies_per_fiber_size, \
         exclusion_epsilon = exclusion_radius, epsilon=patrol_radius, rank_criterion=rank_criterion)
-        
+
         # Reset the colliding fibers
         n_reset = reset_fiber_collisions(id_matching_gals, fibers_x, fibers_y, fibers_x0, fibers_y0, epsilon=exclusion_radius)
 
@@ -353,12 +368,14 @@ def make_fiber_allocation(fibers, gals, tile_visit_ID=1, patrol_radius=0.0, rank
             fibers.y[to_allocate[i]] = fibers_y[i]
             if(id_matching_gals[i]!=-1):
                 gal_assigned = gal_assigned +1
-                gals.tile_visit_ID[free_gals[index_matching_gals[i]]] = tile_visit_ID
-             
-        
+                gals.tile_ID[free_gals[index_matching_gals[i]]] = tile_ID
+                gals.visit_ID[free_gals[index_matching_gals[i]]] = visit_ID
+                gals.fiber_ID[free_gals[index_matching_gals[i]]] = fibers.ID[to_allocate[i]]
+
+
         #recalculate the number of fibers that need to be allocated.
         n_to_allocate = np.size(np.where(fibers.allocated_galaxy_ID==-1))
-        n_to_find = np.size(np.where(gals.tile_visit_ID==-1))
+        n_to_find = np.size(np.where(gals.tile_ID==-1))
         iteration = iteration + 1
 
         if(n_reset==0):
--- source/FiberAllocation.py
+++ source/FiberAllocation.py
@@ -1,50 +1,180 @@
 # -*- coding: utf-8 -*-
-import AllocationModule as AM
 import sys
-import numpy as np
 sys.path.append('../../Wrapper/')
+sys.path.append('../../Plotters/')
+sys.path.append('../../Utilities/')
+
+import AllocationModule as AM
+import numpy as np
+import math
+
+import diagnostic_plot as diagplot
+
+import Plotters
 import archive
+import Utilities
+
+
+
 
 def main():
 
-    #load all the inputs
-    data_bank = '../../../data/data_bank.h5'
-    with archive.archive(data_bank, 'r') as ar:
-        # numbers coming from param.ini
-        fiber_pitch 	= np.float_(ar['/fiber/parameters/fiber_pitch'])
-        fiber_size 	= np.float_(ar['/fiber/parameters/fiber_size'])
-        Ndiameter 	= np.int_(ar['/fiber/parameters/num_fibers_on_diameter'])
-        n_pass_per_tile = np.int_(ar['/allocation/parameters/n_pass_per_tile'])
-        patrol_radius   = np.float_(ar['/allocation/parameters/patrol_radius'])
-        allocation_method = ar['/allocation/parameters/allocation_method']
+	print "Started fiber allocation. Importing from data bank..."
+	#load all the inputs
+	data_bank = '../../../data/data_bank.h5'
+	with archive.archive(data_bank, 'r') as ar:
+		# numbers coming from param.ini
+		fiber_pitch 	  = ar['/Fiber_Allocation/fiber_pitch']
+		fiber_size 		  = ar['/Fiber_Allocation/fiber_size']
+		Ndiameter 		  = ar['/Fiber_Allocation/num_fibers_on_diameter']
+		n_pass_per_tile   = ar['/Fiber_Allocation/n_pass_per_tile']
+		patrol_radius	 = ar['/Fiber_Allocation/patrol_radius']
+		allocation_method = ar['/Fiber_Allocation/allocation_method']
 
-        # numbers coming from the previous step in the pipeline
-        gals_x_in	= ar['/gal/ra_true']		
-        gals_y_in	= ar['/gal/dec_true']		
-        gals_id_in	= ar['/gal/galaxy_index']
-        number_of_tiles = ar['/tiling/number_of_tiles'] 
-        tiles_centers_ra = ar['/tiling/tile_centers_ra'] 
-        tiles_centers_dec = ar['/tiling/tile_centers_dec'] 
-        tile_observation_time =ar['/tiling/tile_observation_time'] 
-        
-    #initialize the galaxy structures
-    all_gals = AM.MockGalaxyCatalog(x_in=gals_x_in, y_in=gals_y_in, id_in=gals_id_in)
+		# numbers coming from the previous step in the pipeline
+		gals_x_in				= ar['/gal/ra_true']
+		gals_y_in				= ar['/gal/dec_true']
+		gals_id_in				= ar['/gal/galaxy_index']
+		number_of_tiles				= ar['/tiling/number_of_tiles']
+		tiles_centers_ra		= ar['/tiling/tile_centers_ra']
+		tiles_centers_dec			= ar['/tiling/tile_centers_dec']
+		galaxy_tile_id_list			= ar['/gal/tile_ID']
+		tile_id_list			= ar['/tiling/tile_ID']
+		survey_selection_flag = ar['/gal/survey_selection_flag']
 
-    #loop over the tiles
-    tile_ID = 0
-    for ra_center, dec_center in zip(tiles_centers_ra, tiles_centers_dec):
-        for pass_ID in range(n_pass_per_tile):
-        # create the unperturbed set of fibers for this pass in this tile
-            fibers = AM.FiberSet(Ndiameter=Ndiameter, fiber_pitch=fiber_pitch,\
-                             fiber_size=fiber_size, center_x=ra_center, center_y=dec_center)
-            AM.make_fiber_allocation(fibers, all_gals, tile_visit_ID=tile_ID, rank_criterion=allocation_method,\
-                                     patrol_radius=fibers.fiber_pitch, exclusion_radius=fibers.fiber_size * 0.5)
+		survey_selection_flag = np.array(survey_selection_flag)
+		gals_x_in = gals_x_in[survey_selection_flag]
+		gals_y_in = gals_y_in[survey_selection_flag]
+		gals_id_in = gals_id_in[survey_selection_flag]
+		galaxy_tile_id_list = galaxy_tile_id_list[survey_selection_flag]
 
-        #update the tile_ID
-        tile_ID = tile_ID + 1
 
-# ===================================================== 
+
+	print "...done."
+
+
+	selected = np.where(galaxy_tile_id_list>0)
+	n_in_tiles = np.size(selected)
+	print 'There are %d galaxies in tiles'%(n_in_tiles)
+	print 'Min-Max RA Tiles: %f %f'%(np.amin(tiles_centers_ra), np.amax(tiles_centers_ra))
+	print 'Min-Max DEC Tiles: %f %f'%(np.amin(tiles_centers_dec), np.amax(tiles_centers_dec))
+	print 'Min-Max RA Galaxies: %f %f'%(np.amin(gals_x_in), np.amax(gals_x_in))
+	print 'Min-Max DEC Galaxies: %f %f'%(np.amin(gals_y_in), np.amax(gals_y_in))
+
+#	plot_name = "all_galaxies"
+#	diagplot.plot_positions(gals_x_in[selected], gals_y_in[selected], plot_name)
+
+	# redefine the positions of the galaxies and tile centers to a 'planar' frame
+	gals_x_in = gals_x_in * np.cos(gals_y_in * math.pi / 180.0)
+	tiles_centers_ra  = tiles_centers_ra * np.cos(tiles_centers_dec * math.pi / 180.0)
+
+	#initialize full galaxy structure
+	all_gals = AM.MockGalaxyCatalog(x_in=gals_x_in, y_in=gals_y_in, id_in=gals_id_in)
+
+	fraction_allocated = np.empty((0))
+
+	#loop over the tiles
+	for ra_center, dec_center, tile_id in zip(tiles_centers_ra, tiles_centers_dec, tile_id_list):
+		# create the unperturbed set of fibers for this pass in this tile
+		fibers = AM.FiberSet(Ndiameter=Ndiameter, fiber_pitch=fiber_pitch,\
+						 fiber_size=fiber_size, center_x=ra_center, center_y=dec_center)
+
+		# set to -1 the fiber_ID of the galaxies that should fall inside a tile
+		index_inside = np.where((galaxy_tile_id_list==tile_id) & (all_gals.tile_ID < 0))
+		all_gals.tile_ID[index_inside] = -1
+
+		if np.size(index_inside)>0:
+			print "Galaxies to allocate", np.size(index_inside)
+
+			# Finally. Make the allocation!
+			AM.make_fiber_allocation(fibers, all_gals, tile_ID=tile_id, visit_ID=1, rank_criterion=allocation_method,\
+						 patrol_radius=fibers.fiber_pitch, exclusion_radius=fibers.fiber_size * 0.5)
+
+			n_alloc = np.where(all_gals.fiber_ID>0)
+
+		#update the number of allocated galaxies for a control plot
+		gal_alloc = np.where(all_gals.fiber_ID>0)
+		n_total = np.size(all_gals.fiber_ID)
+		n_alloc = np.size(gal_alloc)
+		fraction_allocated = np.append(fraction_allocated, (1.0 * n_alloc / (1.0*n_total)))
+
+
+
+	# final stats
+	gal_alloc = np.where(all_gals.fiber_ID>0)
+	n_total = np.size(all_gals.fiber_ID)
+	n_alloc = np.size(gal_alloc)
+
+	print "Allocation efficiency", (1.0*n_alloc)/(1.0*n_total)
+	print "Initial number of galaxies (after target selection)", n_total
+	print "Initial number of galaxies (in tiles)", n_in_tiles
+	print "Galaxies that were allocated:", n_alloc
+
+
+
+	inner_fiber_selection_flag = np.array((all_gals.fiber_ID > 0))
+	print "survey selection:", survey_selection_flag.shape, np.sum(survey_selection_flag)
+	print "inner fiber selection:", inner_fiber_selection_flag.shape, np.sum(inner_fiber_selection_flag)
+
+	fiber_selection_flag = survey_selection_flag
+	fiber_selection_flag[survey_selection_flag] = inner_fiber_selection_flag
+	print "fiber selection:", fiber_selection_flag.shape, np.sum(fiber_selection_flag)
+
+	# Write result to data bank
+	with archive.archive(data_bank, 'a') as ar:
+		ar['/gal/tile_id'] = all_gals.tile_ID
+		ar['/gal/visit_id'] = all_gals.visit_ID
+		ar['/gal/fiber_id'] = all_gals.fiber_ID
+		ar['/gal/fiber_selection_flag'] = fiber_selection_flag
+
+
+
+	# =====================================================
+	# Diagnostics
+	# =====================================================
+	print '... diagnostic plots'
+
+	# fiberid distribution
+	try:
+		check_fiber_sel = numpy.where( fiber_selection_flag  == True)[0]
+		fiber_id_sub = all_gals.fiber_ID[check_fiber_sel]
+		width, center, bin_edges, hist = Utilities.make_histogram(fiber_id_sub, int(float(Ngal)/10.))
+		Plotters.plot_fa_hist_fiberid(center, width, hist,
+									   fig_number=0, plot_number=0,  title='n(fiberid)', base_directory='.', show_plot=True)
+	except (RuntimeError, TypeError, NameError):
+		print 'bad plot'
+		pass
+
+
+	# galaxy positions for those that are allocated (ra/dec position plot)
+	try:
+		Plotters.plot_fa_gal_position(all_gals.x[gal_alloc],all_gals.y[gal_alloc],
+									 fig_number=1, plot_number=0,  title='galaxy position', base_directory='.', show_plot=True)
+	except (RuntimeError, TypeError, NameError):
+		print 'bad plot'
+		pass
+
+	# fraction allocated vs. tile number
+	try:
+		tile_list = np.arange(np.size(fraction_allocated))
+		Plotters.plot_fa_completeness(tile_list, fraction_allocated,
+									   fig_number=2, plot_number=0,  title='fiber completeness', base_directory='.', show_plot=False)
+	except (RuntimeError, TypeError, NameError):
+		print 'bad plot'
+		pass
+
+	#plot_name = "galaxies_with_fiber"
+	#diagplot.plot_positions(all_gals.x[gal_alloc],all_gals.y[gal_alloc],plot_name)
+
+	#plot_name = "completeness fraction"
+	#tile_list = np.arange(np.size(fraction_allocated))
+	#diagplot.plot_fraction(tile_list,fraction_allocated, plot_name)
+
+
+
+
+# =====================================================
 if __name__ == '__main__':
-    main()
+	main()
 
 
